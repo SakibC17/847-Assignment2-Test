b2c33a4f46bf7b1c1c1d6498a2ef747cfcc39bfe
Sakib C <67219356+ZeeboSC17@users.noreply.github.com>
2024-03-31 13:27:04 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,6 @@ import Home from './components/Home.vue'
 import { format, compareAsc } from "date-fns";
 import { ref, watchEffect } from 'vue'
 
-const date = format(new Date(), "MM/dd/yyyy hh:mm:ss");
-const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=`;
-const cityCountry = 'Belleville,ca';
-const API_KEY = '&appid=0dac45f986747aaa0880a63fbf028b9a';
-const units = '&units=metric'
-
-const commits = ref(null)
-
-watchEffect(async () => {
-  const url = `${API_URL}${cityCountry}${API_KEY}${units}`
-  commits.value = await (await fetch(url)).json()
-})
 
 </script>
 <template>
